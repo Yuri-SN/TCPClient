c4d2739fc4977afc95a144b2890ec213206998aa
Yuri Solyanik <jura.s.n@gmail.com>
2025-06-15 04:32:51 +0500

    Добавил прекод

--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,8 @@
 
 #include <QMainWindow>
 
+#include "tcpclient.h"
+
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -16,6 +18,21 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pb_connect_clicked();
+    void on_pb_request_clicked();
+
+    void DisplayTime(QDateTime time);
+    void DisplayFreeSpace(uint32_t freeSpace);
+    void SetDataReply(QString);
+    void DisplayStat(StatServer);
+    void DisplayConnectStatus(uint16_t status);
+    void DisplayError(uint16_t error);
+    void DisplaySuccess(uint16_t typeMess);
+
+    void on_cb_request_currentIndexChanged(int index);
+
 private:
     Ui::MainWindow *ui;
+    TCPclient *client;
 };
